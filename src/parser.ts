246c04263b08e7b3d6ff733235a6ee23969cc2bd
zhaizhe <zhaizhe@didichuxing.com>
2019-07-23 22:24:23 +0800

    feat(parser): make function

--- src/parser.ts
+++ src/parser.ts
@@ -1,47 +1,53 @@
-import path from 'path';
+import path, { parse } from 'path';
 import lineReader from 'line-reader';
 
 const filePath = path.join(__dirname, '../data/example.txt');
-
-let currentDate;
-let lineItem;
-const events = [];
-let lineNumber = 0;
-lineReader.eachLine(filePath, (line, isLast) => {
-  try {
-    lineNumber++;
-    // trim line
-    const trimedLine = line.replace(/\s/g, '');
-    // test if the line is a date
-    lineItem = trimedLine.match(/\d{4}-\d{2}-\d{2}/);
-    if (lineItem && lineItem.length === 1) {
-      currentDate = trimedLine.match(/\d{4}-\d{2}-\d{2}/)[0];
-    }
-    if (trimedLine.includes('|')) {
-      const [timePeriod, category, description] = trimedLine.split('|');
-      if (!(timePeriod && category && description)) {
-        throw(new Error(`Log format error in line ${lineNumber}, please check \n\r line content: ${line}`));
-      }
-      const [start, end] = timePeriod.match(/\d{2}:\d{2}/g);
-      if (!(start && end)) {
-        throw(new Error(`Log format error in line ${lineNumber}, please check \n\r line content: ${line}`));
-      }
-      events.push({
-        date: currentDate,
-        start,
-        end,
-        category,
-        description,
-      });
-    }
-    if (isLast) {
-      handleReadFileFinish();
-    }
-  } catch (err) {
-    console.log(err.message)
-  }
+parser(filePath).then((data) => {
+  console.log(data);
 });
 
-function handleReadFileFinish() {
-  // console.log(events);
+function parser(filePath) {
+  return new Promise((resolve, reject) => {
+    let currentDate;
+    let lineItem;
+    const events = [];
+    let lineNumber = 0;
+    lineReader.eachLine(filePath, (line, isLast) => {
+      try {
+        lineNumber++;
+        const errorMsg = `Log format error in line ${lineNumber}, please check \n\r line content: ${line}`;
+        // trim line
+        const trimedLine = line.replace(/\s/g, '');
+        // test if the line is a date
+        lineItem = trimedLine.match(/\d{4}-\d{2}-\d{2}/);
+        if (lineItem && lineItem.length === 1) {
+          currentDate = trimedLine.match(/\d{4}-\d{2}-\d{2}/)[0];
+        }
+        // if line is a event line
+        if (trimedLine.includes('|')) {
+          const [timePeriod, category, description] = trimedLine.split('|');
+          if (!(timePeriod && category && description)) {
+            throw(new Error(errorMsg));
+          }
+          const [start, end] = timePeriod.match(/\d{2}:\d{2}/g);
+          if (!(start && end)) {
+            throw(new Error(errorMsg));
+          }
+          events.push({
+            date: currentDate,
+            start,
+            end,
+            category,
+            description,
+          });
+        }
+        // handle finish
+        if (isLast) {
+          resolve({events});
+        }
+      } catch (error) {
+        resolve({error})
+      }
+    });
+  });
 }
